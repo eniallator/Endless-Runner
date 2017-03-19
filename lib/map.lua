d08b86f20cf50e7e0a332db96b19dca006cad51f
eniallator <eniallatormc@gmail.com>
2017-03-19 08:35:33 +0000

    Added creation of buildings in the map, on to collision next

--- lib/map.lua
+++ lib/map.lua
@@ -1,11 +1,49 @@
 local map = {}
+map.moveSpeed = 10
+map.buildings = {}
+map.progress = 0
+local noiseX = 0
 
-map.update = function(dt)
+local function createBuilding()
+  local building = {}
+  building.w = math.random() * screenDim.x / 2 + screenDim.x * (3 / 4)
+  building.h = love.math.noise(noiseX) * screenDim.y / 2 + screenDim.y / 6
 
+  noiseX = noiseX + 0.5
+  return building
+end
+
+local function cullBuildings()
+  while map.buildings[1] and map.buildings[1].w - map.progress < 0 do
+    table.remove(map.buildings, 1)
+    map.progress = - screenDim.x / 9
+  end
+end
+
+local function populateBuildings()
+  while #map.buildings < 2 do
+    table.insert(map.buildings, createBuilding())
+  end
+end
+
+map.update = function()
+  cullBuildings()
+  populateBuildings()
+
+  map.progress = map.progress + map.moveSpeed
 end
 
 map.draw = function()
+  local offset = 0
 
+  for i=1, #map.buildings do
+    local building = map.buildings[i]
+    print()
+
+    love.graphics.setColor(0, 255, 0)
+    love.graphics.rectangle("fill", -map.progress + offset, screenDim.y - building.h, building.w, building.h)
+    offset = offset + building.w + screenDim.x / 9
+  end
 end
 
 return map
--- main.lua
+++ main.lua
@@ -3,12 +3,15 @@ function love.load()
   screenDim.x, screenDim.y = love.graphics.getDimensions()
 
   player = require "lib.player"
+  map = require "lib.map"
 end
 
 function love.update()
+  map.update()
   player.update()
 end
 
 function love.draw()
+  map.draw()
   player.draw()
 end
